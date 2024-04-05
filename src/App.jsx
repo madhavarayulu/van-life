e0d9ad23593f11a89bd8294ff2a6b9ea1b475f89
madhavarayulu <madhavarayulu@gmail.com>
2024-04-05 23:00:57 +0530

    set up project locally and push to remote

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className='site-logo' to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
